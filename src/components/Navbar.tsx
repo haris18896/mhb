a1d1235b034e940df54678d9f19ba8918f651f2f
haris18896 <haris18896@gmail.com>
2024-12-11 02:18:04 +0500

    contact form has been removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     {name: 'Home', path: '/'},
     {name: 'About', path: '/about'},
     {name: 'Services', path: '/services'},
-    {name: 'Contact', path: '/contact'},
+    // {name: 'Contact', path: '/contact'},
   ];
 
   return (
